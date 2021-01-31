63d45fba8b53ff80626578d4b6b8c5d06a17c960
João Marques <jahp94@gmail.com>
2021-01-31 12:47:56 +0000

    update

---
+++ GGJ2021/Assets/Scripts/MusicBackground.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBackground : MonoBehaviour
+{
+
+    // [FMODUnity.EventRef]
+    // public string STPath= String.Empty;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     soundTrack = FMODUnity.RuntimeManager.CreateInstanceSTPath= 
+    //     soundTrack.start();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    // void onDestroy() {
+    //     soundTrack.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    //     soundTrack.release();
+    // }
+
+
+// quando_proximo_de_enimigos {
+// soundTrack.start.setParameterByName("inBattle", 1);
+// else {
+// soundTrack.start.setParameterByName("inBattle", 0);
+
+
+}
